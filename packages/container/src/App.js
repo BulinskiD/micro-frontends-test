c9d315ec0b36fef40dfba7ee8078c7f30e8f71a4
Dawid Bulinski (PGS Software) <dbulinski@pgs-soft.com>
2021-01-06 20:09:56 +0100

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <h1>Hello there</h1>
+      <MarketingApp />
+    </React.Fragment>
+  );
+}
